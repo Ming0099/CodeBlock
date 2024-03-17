1143d145c1c9bdf7a2617ba927cb59ffe1884d6a
Ming0099 <130200978+Ming0099@users.noreply.github.com>
2024-03-17 19:16:12 +0900

    번역기 공통 기능 분리

--- src/main/java/inje/codeblock/service/translator/CTranslator.java
+++ src/main/java/inje/codeblock/service/translator/CTranslator.java
@@ -2,7 +2,7 @@ package inje.codeblock.service.translator;
 
 import java.util.Stack;
 
-public class CTranslator implements CodeTranslator{
+public class CTranslator extends TranslatorFunction implements CodeTranslator{
     private StringBuilder code;
     private Stack<Integer> codeDepthStack;
 
@@ -14,15 +14,11 @@ public class CTranslator implements CodeTranslator{
 
     @Override
     public void translateVariable(String name, String value) {
-        String type = "";
-        if (isInteger(value)) {
-            type = "int";
-        } else if (isDouble(value)) {
-            type = "double";
-        } else if (isCharacter(value)) {
-            type = "char";
-        } else {
-            type = "char *";
+        String type = translateType(value);
+        if (type.equals("char")){ // 문자
+            value = "\'" + value + "\'";
+        }else if(type.equals("char *")){ // 문자열
+            value = "\"" + value + "\"";
         }
         createIndent(getCodeDepth());
         code.append(type).append(" ").append(name).append(" = ").append(value).append(";\n");
@@ -52,26 +48,8 @@ public class CTranslator implements CodeTranslator{
 
     @Override
     public void translateIf(String s1, String condition, String s2) {
-        switch (condition){
-            case "작다":
-                condition = "<";
-                break;
-            case "크다":
-                condition = ">";
-                break;
-            case "같다":
-                condition = "==";
-                break;
-            case "작거나 같다":
-                condition = "<=";
-                break;
-            case "크거나 같다":
-                condition = ">=";
-                break;
-            case "다르다":
-                condition = "!=";
-                break;
-        }
+        // 조건 변환
+        condition = translateCondition(condition);
         createIndent(getCodeDepth());
         code.append("if(").append(s1).append(" ").append(condition).append(" ").append(s2).append(") {\n");
         codeDepthStack.push(codeDepthStack.peek() + 1);
@@ -107,30 +85,5 @@ public class CTranslator implements CodeTranslator{
     public String getTranslatedCode() {
         closeAllBraces();
         return code.toString();
-    }
-
-    // 정수 확인
-    public static boolean isInteger(String input) {
-        try {
-            Integer.parseInt(input);
-            return true;
-        } catch (NumberFormatException e) {
-            return false;
-        }
-    }
-
-    // 소수 확인
-    public static boolean isDouble(String input) {
-        try {
-            Double.parseDouble(input);
-            return true;
-        } catch (NumberFormatException e) {
-            return false;
-        }
-    }
-
-    // 문자 확인
-    public static boolean isCharacter(String input) {
-        return input.length() == 1;
     }
 }
